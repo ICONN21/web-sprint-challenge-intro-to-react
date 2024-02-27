9cb2e3072b469e8ce882112728af2db327272161
Ian Connor <Ian.connor0921@gmail.com>
2024-02-27 16:58:53 -0500

    finished sprint6 challenge

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -6,15 +6,39 @@ const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
+ const [people, setPeople] = useState([])
+ const [planets,setPlanets] = useState([])
+
+  useEffect(() => {
+   async function fetchData() {
+      try {
+        const peopleResponse = await axios.get(urlPeople);
+        const planetResponse = await axios.get(urlPlanets)
+
+      setPeople(peopleResponse.data)
+      setPlanets(planetResponse.data);
+      
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchData()
+  }, [])
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
-    </div>
-  )
+    
+      {people.map(person => {
+        const homeworld = planets.find(planet => planet.id === person.homeworld)
+        
+        return <Character key={person.id} data = {{...person,homeworldName: homeworld.name}} />
+      
+})}
+    </div>       
+  );
 }
 
 export default App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ data }) { // ❗ Add the props
+  const { name, homeworldName } = data
+
+  const [showHomeworld, setShowHomeworld] = useState(false);
+
+  const toggleHomeworld = () => {
+    setShowHomeworld(!showHomeworld);
+  }
+  
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
+    <div className='character-card' onClick={toggleHomeworld}>
+      <h3 className='character-name'>
+      {name}
+      </h3>
+
+      {showHomeworld && (
+        
+          <p>
+            Planet:  
+            <span className="character-planet"> {homeworldName}</span>
+          </p>
+          
+      )}
       {/* Use the same markup with the same attributes as in the mock */}
     </div>
+    
   )
-}
+  
+  }
 
 export default Character
